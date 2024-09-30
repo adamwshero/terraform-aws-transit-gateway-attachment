1d9a904d2f70a63c44f23fdb8433d3df3839e7f6
Mate Gogiberidze <101423812+mategogiberidze@users.noreply.github.com>
2024-09-30 10:43:05 +0400

    ADD: security_group_referencing_support in resource aws_ec2_transit_gateway_vpc_attachment for aws 5.69.0 version

--- main.tf
+++ main.tf
@@ -69,12 +69,14 @@ locals {
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
   for_each = { for k, v in var.transit_gateway_attachments : k => v if var.create_attachment }
 
-  vpc_id                 = each.value.vpc_id
-  subnet_ids             = each.value.subnet_ids
-  transit_gateway_id     = each.value.transit_gateway_id
-  appliance_mode_support = var.appliance_mode_support
-  dns_support            = var.dns_support
-  ipv6_support           = var.ipv6_support
+  vpc_id                             = each.value.vpc_id
+  subnet_ids                         = each.value.subnet_ids
+  transit_gateway_id                 = each.value.transit_gateway_id
+  appliance_mode_support             = var.appliance_mode_support
+  dns_support                        = var.dns_support
+  ipv6_support                       = var.ipv6_support
+  security_group_referencing_support = var.security_group_referencing_support
+
 
   transit_gateway_default_route_table_association = var.transit_gateway_default_route_table_association
   transit_gateway_default_route_table_propagation = var.transit_gateway_default_route_table_propagation
